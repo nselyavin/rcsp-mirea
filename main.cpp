590338ee23fa663fd2b78b841ee5702733ca7afc
Nikita Selyavin <43264922+Fume5678@users.noreply.github.com>
2022-11-03 22:47:36 +0300

    pract_3 lab 1

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include "rxcpp/rx.hpp"
+#include <chrono>
+#include <exception>
+#include <operators/rx-observe_on.hpp>
+#include <rx-observable.hpp>
+#include <rx-observer.hpp>
+#include <rx-subscriber.hpp>
+#include <thread>
+
+
+using ms = std::chrono::milliseconds;
+
+int main()
+{
+    auto temp_source = rxcpp::observable<>::interval(std::chrono::milliseconds(1000), rxcpp::observe_on_new_thread())
+    .map([](int v){return rand() % 6 + 22;})
+    .publish();
+
+    auto co2_source = rxcpp::observable<>::interval(std::chrono::milliseconds(1000), rxcpp::observe_on_new_thread())
+    .map([](int v){return rand() % 6 + 67;})
+    .publish();
+
+    // Subscribe from the beginning
+    co2_source.subscribe(
+        [](long v){
+            if(v > 70){
+                printf("ALARM: CO2: %ld > 70\n", v);
+            } 
+        },
+        [](){printf("[1] OnCompleted\n");});
+
+    // Another subscription from the beginning
+    decltype(temp_source.subscribe()) sub = temp_source.subscribe(
+        [&sub](long v){
+            if (v > 25){
+                printf("ALARM: temp %ld > 25\n", v);
+            }
+        },
+        [](){printf("[2] OnCompleted\n");});
+
+
+    co2_source.connect();
+    temp_source.connect();
+
+    while(sub.is_subscribed()){
+
+    }
+
+    return 0;
+}

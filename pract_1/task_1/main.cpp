efed5ab8500b8e339204519a486ab1e1a8598315
Nikita Selyavin <nselyavin@inbox.ru>
2022-12-01 11:03:21 +0300

    pract_1 init

---
+++ pract_1/task_1/main.cpp
@@ -0,0 +1,54 @@
+#include <chrono>
+#include <iostream>
+
+#include <omp.h>
+
+#include <thread>
+
+using namespace std::literals::chrono_literals;
+
+int find_max(int* arr, int n){
+    int max = arr[0];
+
+    for(int i = 0; i < n; i++){
+        std::this_thread::sleep_for(1ms);
+        if (max < arr[i]){
+            std::this_thread::sleep_for(1ms);
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+int thread_find_max(int* arr, int n){
+
+}
+
+
+int omp_find_max(int* arr, int n){
+    int max = arr[0];
+
+    return max;
+}
+
+
+int main()
+{
+    const int n = 500;
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        arr[i] = rand() % 200;
+    }
+
+    auto start = std::chrono::system_clock::now();
+    std::cout << "simple find max: " << find_max(arr, n) << std::endl;
+    auto end = std::chrono::system_clock::now();
+    std::cout << "duration: " << std::chrono::duration<double>(end-start).count() << " ms\n";
+
+    // std::cout << "thread find max: " << thread_find_max(arr, n) << std::endl;
+    // std::cout << "omp find max: " << omp_find_max(arr, n) << std::endl;
+
+    return 0;
+}
---
+++ pract_1/task_4/Handler.h
@@ -0,0 +1,46 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+#include <thread>
+#include <vector>
+#include <functional>
+#include <rxcpp/rx.hpp>
+#include <rx-observable.hpp>
+#include <rx-observer.hpp>
+
+#include "File.h"
+
+
+class FileHandler
+{
+public:
+    FileHandler(File f){
+        std::this_thread::sleep_for(std::chrono::milliseconds(f.size() * 10));
+        if(f.type() == "JSON"){
+            json_handler(f);
+        }
+        if(f.type() == "XLS"){
+            xls_handler(f);
+        }
+        if(f.type() == "XML"){
+            xml_handler(f);
+        }
+    }
+
+    void json_handler(File f){
+        std::cout << "json handler: " << f.size() << ", " << f.type() << std::endl;
+    }
+
+    void xml_handler(File f){
+        std::cout << "xml handler: " << f.size() << ", " << f.type() << std::endl;
+    }
+
+    void xls_handler(File f){
+        std::cout << "xls handler: " << f.size() << ", " << f.type() << std::endl;
+    }
+
+private:
+
+};
+
+#endif // HANDLER_H
